78c9bdb25af3712da86166d2dd3270addfe55739
Varshith-p <varshithpuligadda@gmail.com>
2023-03-23 11:10:54 +0530

    work updated

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Wrapper from "./Wrapper";
 import image from "../assets/image.svg";
+import FundMe from "../assets/FundMe.png";
+import JobTracker from "../assets/JobTracker.png";
+import Portfolio from "../assets/Portfolio.png";
 import { AiFillGithub } from "react-icons/ai";
 import { BiLink } from "react-icons/bi";
 
@@ -10,26 +13,41 @@ const Work = () => {
       <h1 className="text-4xl md:text-8xl font-medium md:ml-10 pb-4 text-[#06b6d4]">
         Work
       </h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 w-4/5 m-auto md:my-20 md:px-8 lg:px-16 gap-16">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 m-auto md:my-20 md:px-8 lg:px-16 gap-16 lg:gap-14">
         <div className="bg-white shadow-md p-3 rounded-md">
-          <img src={image} alt="logo" className="rounded-md" />
+          <div className="flex w-72 h-48 justify-center items-center">
+            <img src={FundMe} alt="logo" className="rounded-md w-full h-full" />
+          </div>
           <div className="flex gap-3 items-center pt-2 text-lg">
-            <p className="flex-1">Job Tracker</p>
+            <p className="flex-1 font-medium">FundMe</p>
             <a
-              href="https://github.com/Varshith-p/job-tracker-mern"
+              href="https://github.com/Varshith-p/blockchain-crowd-funding-app-backend"
               target="_blank"
             >
               <AiFillGithub />
             </a>
-            <a href="https://job-tracker-mern.cyclic.app/" target="_blank">
+            <a href="https://fundmeblockchain.vercel.app/" target="_blank">
               <BiLink />
             </a>
           </div>
+          <div>
+            <span className="font-medium">Frontend:</span> HTML, CSS, JS
+          </div>
+          <div>
+            <span className="font-medium">Backend: </span> Solidity, Ethers.js,
+            Hardhat, Blockchain
+          </div>
         </div>
         <div className="bg-white shadow-md p-3 rounded-md">
-          <img src={image} alt="logo" className="rounded-md" />
+          <div className="flex w-72 h-48 justify-center items-center">
+            <img
+              src={JobTracker}
+              alt="logo"
+              className="rounded-md w-full h-full"
+            />
+          </div>
           <div className="flex items-center pt-2 gap-3 text-lg">
-            <p className="flex-1">Job Tracker</p>
+            <p className="flex-1 font-medium">Job Tracker</p>
             <a
               href="https://github.com/Varshith-p/job-tracker-mern"
               target="_blank"
@@ -40,20 +58,38 @@ const Work = () => {
               <BiLink />
             </a>
           </div>
+          <div>
+            <span className="font-medium">Frontend:</span> HTML, CSS, JS,
+            React.js
+          </div>
+          <div>
+            <span className="font-medium">Backend: </span> Node.js, Express,
+            MongoDB, JWT
+          </div>
         </div>
         <div className="bg-white shadow-md p-3 rounded-md">
-          <img src={image} alt="logo" className="rounded-md" />
+          <div className="flex w-72 h-48 justify-center items-center">
+            <img
+              src={Portfolio}
+              alt="logo"
+              className="rounded-md w-full h-full"
+            />
+          </div>
           <div className="flex gap-3 items-center pt-2 text-lg">
             <p className="flex-1">Job Tracker</p>
             <a
-              href="https://github.com/Varshith-p/job-tracker-mern"
+              href="https://github.com/Varshith-p/react-portfolio"
               target="_blank"
             >
               <AiFillGithub />
             </a>
-            <a href="https://job-tracker-mern.cyclic.app/" target="_blank">
+            <a href="https://varshith-portfolio.vercel.app/" target="_blank">
               <BiLink />
             </a>
+          </div>
+          <div>
+            <span className="font-medium">Technologies:</span> HTML, CSS, JS,
+            React.js
           </div>
         </div>
       </div>
